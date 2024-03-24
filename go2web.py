7aead920d9d0768223a2b1fdf04a9a9c08909e86
Dr.Killer <ahmed-2008-04@hotmail.com>
2024-03-25 00:25:49 +0200

    add basic fuctionality

---
+++ go2web.py
@@ -0,0 +1,55 @@
+import requests
+import argparse
+import webbrowser
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-u", "--url", help="make an HTTP request to the specified URL and print the response")
+parser.add_argument("-s", "--search", help="make an HTTP request to search the term using your favorite search engine and print top 10 results")
+
+args = parser.parse_args()
+
+
+if args.url:
+    response = requests.get(args.url)
+
+    if response.status_code == 200:
+        print(f"GET request successful! , Status_code = {response.status_code}")
+
+    else:
+        print(f"Erorr, Status code: {response.status_code}")
+
+
+
+if args.search:
+    headers_Get = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1'
+        }
+
+
+    def google(q):
+        s = requests.Session()
+        q = '+'.join(q.split())
+        url = 'https://www.google.com/search?q=' + q + '&oq=+' + q + '+&ie=UTF-8'
+        r = s.get(url, headers=headers_Get)
+        soup = BeautifulSoup(r.text, "html.parser")
+        output = []
+        for searchWrapper in soup.find_all('h3'): #this line may change in future based on google's web page structure
+            search_tag = searchWrapper.text
+            search_link = searchWrapper.findPrevious('a').get('href')
+            result = {'text': search_tag, 'link': search_link}
+            output.append(result)
+
+
+        return output
+
+    for i in google(args.search):
+        print(i)
+
